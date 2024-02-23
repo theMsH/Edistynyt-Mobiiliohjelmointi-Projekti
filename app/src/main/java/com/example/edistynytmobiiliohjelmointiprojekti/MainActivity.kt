4152b2c1de9581ee71b0b065b4f49dbf6feb13d5
Miika Haapanen <miika.haapanen@edu.lapinamk.fi>
2024-02-23 21:47:40 +0200

    Created Drawerlayout and NavController

--- app/src/main/java/com/example/edistynytmobiiliohjelmointiprojekti/MainActivity.kt
+++ app/src/main/java/com/example/edistynytmobiiliohjelmointiprojekti/MainActivity.kt
@@ -3,44 +3,120 @@ package com.example.edistynytmobiiliohjelmointiprojekti
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.edistynytmobiiliohjelmointiprojekti.ui.theme.EdistynytMobiiliohjelmointiProjektiTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             EdistynytMobiiliohjelmointiProjektiTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                    val scope = rememberCoroutineScope()
+                    
+                    ModalNavigationDrawer(
+                        drawerState = drawerState,
+                        drawerContent = {
+                            ModalDrawerSheet(modifier = Modifier.fillMaxWidth(0.8f)) {
+                                Spacer(modifier = Modifier.height(16.dp))
+                                NavigationDrawerItem(
+                                    modifier = Modifier
+                                        .padding(NavigationDrawerItemDefaults.ItemPadding),
+                                    label = { Text(text = "Categories") },
+                                    selected = true,
+                                    onClick = {
+                                        navController.navigate("categoriesScreen")
+                                        scope.launch {
+                                            drawerState.close()
+                                        }
+                                              },
+                                    icon = {
+                                        Icon(
+                                            imageVector = Icons.Filled.Home,
+                                            contentDescription = "Home"
+                                        )
+                                    }
+                                )
+                                // Test item
+                                NavigationDrawerItem(
+                                    modifier = Modifier
+                                        .padding(NavigationDrawerItemDefaults.ItemPadding),
+                                    label = { Text(text = "Test item 1") },
+                                    selected = false,
+                                    onClick = {
+                                        scope.launch {
+                                        drawerState.close()
+                                    }
+                                              },
+                                    icon = {
+                                        Icon(
+                                            imageVector = Icons.Filled.Home,
+                                            contentDescription = "Test item 1"
+                                        )
+                                    }
+                                )
+                                // Test item
+                                NavigationDrawerItem(
+                                    modifier = Modifier
+                                        .padding(NavigationDrawerItemDefaults.ItemPadding),
+                                    label = { Text(text = "Test item 2") },
+                                    selected = false,
+                                    onClick = {
+                                        scope.launch {
+                                        drawerState.close()
+                                    }
+                                              },
+                                    icon = {
+                                        Icon(
+                                            imageVector = Icons.Filled.Home,
+                                            contentDescription = "Test item 2"
+                                        )
+                                    }
+                                )
+                            }
+                        }
+                    ) { // Navhost
+                        NavHost(
+                            navController = navController,
+                            startDestination = "categoriesScreen"
+                        ) {
+                            composable(route="categoriesScreen") {
+                                T
+                            }
+                        }
+                    } // Navhost end
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EdistynytMobiiliohjelmointiProjektiTheme {
-        Greeting("Android")
     }
 }
