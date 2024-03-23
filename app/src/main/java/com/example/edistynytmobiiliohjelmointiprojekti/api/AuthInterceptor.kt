f222d2ddf1be5ef5eef5a8ed4b8e67f962a048f0
Miika Haapanen <miika.haapanen@edu.lapinamk.fi>
2024-03-23 19:09:09 +0200

    Added comments and function just for clearing token

--- app/src/main/java/com/example/edistynytmobiiliohjelmointiprojekti/api/AuthInterceptor.kt
+++ app/src/main/java/com/example/edistynytmobiiliohjelmointiprojekti/api/AuthInterceptor.kt
@@ -5,22 +5,32 @@ import okhttp3.OkHttpClient
 import okhttp3.Response
 
 
-val authInterceptor = AuthInterceptor("")
+val authInterceptor = AuthInterceptor()
 
+// Create OkHttpClient with AuthInterceptor instance. This is used in Retrofit builder
 val okHttpClient = OkHttpClient.Builder()
     .addInterceptor(authInterceptor)
     .build()
 
 
-class AuthInterceptor(private var token: String) : Interceptor {
+class AuthInterceptor(private var token: String = "") : Interceptor {
+
+    // Set token on login.
     fun setToken(newToken: String) {
         this.token = newToken
     }
 
+    // Clear token on logout. Could be done with setToken too
+    fun clearToken() {
+        this.token = ""
+    }
+
+    // This is used for preventing unauthorized access in this software, even if backend allows it
     fun hasEmptyToken() : Boolean {
         return this.token == ""
     }
 
+    // Headers for requests are added here
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
             .addHeader("Authorization", "Bearer $token")
